ad25189873c43386db49c0311fb46a111ae0b50a
MuFaqihi <mmnouc2@gmail.com>
2025-04-16 16:20:20 +0000

    Add main header: Contains all function prototypes and global variables

--- main.h
+++ main.h
@@ -3,28 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <dirent.h>
 #include <ctype.h>
 
-/* Global environment */
-extern char **environ;
-
-/* Function prototypes */
+/* Function Prototypes */
+int is_blank_line(char *line);
+char *get_input(void);
 char **parse_input(char *input);
 char *find_path(char *command);
 int execute_command(char **args, char *argv0);
 int check_builtins(char **args);
 void free_args(char **args);
-char *get_input(void);
-char *get_env_var(const char *name);
-char *find_command_in_path(char *command);
+
+extern char **environ;
 
 #endif /* MAIN_H */
