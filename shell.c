25f1ac7772f56e0f91052e9c777d8066c6208b3c
MuFaqihi <mmnouc2@gmail.com>
2025-04-16 16:45:55 +0000

    Add shell main: Implements core shell loop and input processing

--- shell.c
+++ shell.c
@@ -2,40 +2,40 @@
 
 int main(int argc, char **argv)
 {
-    char *line = NULL;
-    char **args = NULL;
-    int status;
-    (void)argc;
+char *line = NULL;
+char **args = NULL;
+int status;
+(void)argc;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf(":) ");
+while (1)
+{
+if (isatty(STDIN_FILENO))
+printf(":) ");
 
-        line = get_input();
-        if (!line)
-            break;
+line = get_input();
+if (!line)
+break;
 
-        if (is_blank_line(line))
-        {
-            free(line);
-            continue;
-        }
+if (is_blank_line(line))
+{
+free(line);
+continue;
+}
 
-        args = parse_input(line);
-        if (!args[0])
-        {
-            free_args(args);
-            free(line);
-            continue;
-        }
+args = parse_input(line);
+if (!args[0])
+{
+free_args(args);
+free(line);
+continue;
+}
 
-        status = check_builtins(args);
-        if (status == 0)
-            status = execute_command(args, argv[0]);
+status = check_builtins(args);
+if (status == 0)
+status = execute_command(args, argv[0]);
 
-        free_args(args);
-        free(line);
-    }
-    return (0);
+free_args(args);
+free(line);
+}
+return (0);
 }
