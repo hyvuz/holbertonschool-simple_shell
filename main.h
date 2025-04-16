973568de2c7a96ac5c1ba28b780e8aabb4fa6ada
MuFaqihi <mmnouc2@gmail.com>
2025-04-16 13:42:11 +0000

    Add main.h: Main header file containing all function prototypes, standard libraries and global variables like 'environ' for environment variables

--- main.h
+++ main.h
@@ -1,23 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Author: Haneen Aldawood, Munirah Faqihi
- * Description: Header file for the simple shell project.
- * Contains function prototypes and standard libraries.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <dirent.h>
+#include <ctype.h>
 
+/* Global environment */
 extern char **environ;
-int _strcmp(char *s1, char *s2);
-int _strlen(const char *s);
-int is_blank_line(char *line);
 
-#endif
+/* Function prototypes */
+char **parse_input(char *input);
+char *find_path(char *command);
+int execute_command(char **args, char *argv0);
+int check_builtins(char **args);
+void free_args(char **args);
+char *get_input(void);
+char *get_env_var(const char *name);
+char *find_command_in_path(char *command);
+
+#endif /* MAIN_H */
