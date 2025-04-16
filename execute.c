802c83b07e1314152472846232ba2bbd62746550
MuFaqihi <mmnouc2@gmail.com>
2025-04-16 16:50:31 +0000

    Add command execution: Handles builtins (exit, env) and process forking

--- execute.c
+++ execute.c
@@ -23,6 +23,7 @@ return (1);
 /* Add more built-ins here as needed */
 return (0);
 }
+
 /**
  * execute_command - Executes a command with arguments
  * @args: Array of command and arguments
@@ -31,47 +32,47 @@ return (0);
  */
 int execute_command(char **args, char *argv0)
 {
-    char *path;
-    pid_t pid;
-    int status;
+char *path;
+pid_t pid;
+int status;
 
-    if (!args || !args[0])
-        return (-1);
+if (!args || !args[0])
+return (-1);
 
-    /* Handle built-in commands */
-    if (check_builtins(args) == 1)
-        return (1);
+/* Handle built-in commands */
+if (check_builtins(args) == 1)
+return (1);
 
-    /* Handle path resolution */
-    path = (args[0][0] == '/') ? strdup(args[0]) : find_path(args[0]);
+/* Handle path resolution */
+path = (args[0][0] == '/') ? strdup(args[0]) : find_path(args[0]);
 
-    if (!path)
-    {
-        fprintf(stderr, "%s: 1: %s: not found\n", argv0, args[0]);
-        return (-1);
-    }
+if (!path)
+{
+fprintf(stderr, "%s: 1: %s: not found\n", argv0, args[0]);
+return (-1);
+}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execve(path, args, environ) == -1)
-        {
-            perror(argv0);
-            free(path);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid < 0)
-    {
-        perror("fork");
-        free(path);
-        return (-1);
-    }
-    else
-    {
-        wait(&status);
-        free(path);
-    }
+pid = fork();
+if (pid == 0)
+{
+if (execve(path, args, environ) == -1)
+{
+perror(argv0);
+free(path);
+exit(EXIT_FAILURE);
+}
+}
+else if (pid < 0)
+{
+perror("fork");
+free(path);
+return (-1);
+}
+else
+{
+wait(&status);
+free(path);
+}
 
-    return (1);
+return (1);
 }
