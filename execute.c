c423aadd5b1039c9433070460fead60c8ad3218b
MuFaqihi <mmnouc2@gmail.com>
2025-04-23 13:39:44 +0000

    Implement command execution with fork and execve system calls

--- execute.c
+++ execute.c
@@ -1,59 +1,79 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-/**
- * run_command - Executes a command with its arguments
- * @argv: array of argument strings (argv[0] = command)
- * @line: the original input line (to be freed after use)
- *
- * Return: void
- */
-void run_command(char **argv, char *line)
+int execute_cmd(char **args)
 {
-	pid_t pid;
-	int status;
-	char *cmd_path;
+    char *cmd_path;
+    int status;
 
-	/* Defensive check to avoid crash on empty input */
-	if (!argv || !argv[0])
-	{
-		free(line);
-		return;
-	}
+    if (args[0] == NULL)
+        return (0);
 
-	pid = fork();
-	if (pid == 0)
-	{
-		cmd_path = search_path(argv[0]);
+    if (is_builtin(args))
+        return (0);
 
-		if (!cmd_path)
-		{
-			perror("./hsh");
-			exit(EXIT_FAILURE);
-		}
+    cmd_path = resolve_cmd_path(args[0]);
+    if (cmd_path == NULL)
+    {
+        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+        return (127);
+    }
 
-		if (execve(cmd_path, argv, environ) == -1)
-		{
-			perror("./hsh");
-			free(cmd_path);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else if (pid > 0)
-	{
-		wait(&status);
-	}
-	else
-	{
-		perror("fork");
-		free(line);
-		exit(EXIT_FAILURE);
-	}
-
-	free(line);
+    status = run_cmd(cmd_path, args);
+    free(cmd_path);
+    return (status);
 }
 
+int run_cmd(char *cmd_path, char **args)
+{
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        if (execve(cmd_path, args, environ) == -1)
+        {
+            fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+            free(cmd_path);
+            exit(127);
+        }
+    }
+    else if (pid < 0)
+    {
+        perror("fork");
+        return (1);
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status))
+            status = WEXITSTATUS(status);
+        else
+            status = 1;
+    }
+
+    return (status);
+}
+
+int is_builtin(char **args)
+{
+    if (strcmp(args[0], "env") == 0)
+    {
+        int i = 0;
+        while (environ[i])
+            printf("%s\n", environ[i++]);
+        return (1);
+    }
+    return (0);
+}
+
+int handle_exit(char **args, char *line, int last_status)
+{
+    int status = last_status;
+    if (args[1])
+        status = atoi(args[1]);
+
+    free(line);
+    free(args);
+    exit(status);
+}
