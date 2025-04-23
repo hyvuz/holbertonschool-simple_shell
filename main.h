6f69c23779580a655a3e7c0d1447469f5a1c254a
MuFaqihi <mmnouc2@gmail.com>
2025-04-23 14:08:05 +0000

    Revert main.h before fix

--- main.h
+++ main.h
@@ -2,25 +2,12 @@
 #define MAIN_H
 
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
 
 extern char **environ;
 
-char **parse_line(char *line);
 char *read_input(void);
-int execute_cmd(char **args);
-int is_builtin(char **args);
-int handle_exit(char **args, char *line, int last_status);
-char *resolve_cmd_path(char *cmd);
-char *find_command(char *command);
-int run_cmd(char *cmd_path, char **args);
-void print_prompt(void);
-ssize_t read_line(char **line, size_t *len);
+void parse_input(char *line, char **argv);
+void run_command(char **argv, char *line);
+char *search_path(char *command);
 
 #endif /* MAIN_H */
